3667122087189405da212b8b58989a8ae510ddf0
Shubhodeep <banerjeeshubho98@gmail.com>
2023-12-04 16:02:27 +0530

    updates

--- src/components/Priceswitch.jsx
+++ src/components/Priceswitch.jsx
@@ -14,9 +14,22 @@ const Priceswitch = () => {
       <div class="checkbox__container">
         <label for="input-toggle" class="label-for-toggle">
           <span class="ball arrow">
-            <FaArrowCircleRight className='text-4xl text-white' />
+            <FaArrowCircleRight className="text-4xl text-white" />
           </span>
         </label>
+      </div>
+
+      <div className="flex flex-row gap-16 mt-6">
+        <div>
+          <button className="bg-gradient-to-r from-cyan-500 to-purple-500 text-white py-2 px-4 rounded-full shadow-md hover:shadow-lg transition duration-300">
+            Monthly
+          </button>
+        </div>
+        <div>
+          <button className="bg-gradient-to-r from-pink-500 to-cyan-500 text-white py-2 px-4 rounded-full shadow-md hover:shadow-lg transition duration-300">
+            Anually
+          </button>
+        </div>
       </div>
     </div>
   );
